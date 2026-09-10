8b62498b5c783f2360399da198c6f72988dc4be4
atilola-hub <atilolaoluwadara@gmail.com>
2026-09-10 17:01:18 +0100

    update script.js elements

--- script.js
+++ script.js
@@ -1,32 +1,13 @@
-// CALCULATOR PROGRAM.
+// CALCULATOR PROGRAM
 
-// const display = document.getElementById("display");
 const display = document.getElementById("display");
 
-console.log(display);
-
-
-
 function appendToDisplay(input){
-    display.value += input;
 
 }
-
-function clearDisplay(){
-   display.value = "";
-
-}
-
 function calculate(){
-    try{
-        display.value = eval(display.value);
 
-    }
-    catch(error){
-        display.value = "Error";
-    }
-
-        
-    }
-   
-   
+}
+function clearDisplay(){
+    
+}
