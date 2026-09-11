5fb3a08f3b16014d5a567575456782be250c8d66
atilola-hub <atilolaoluwadara@gmail.com>
2026-09-11 20:05:42 +0100

    javascript elements

--- script.js
+++ script.js
@@ -3,11 +3,19 @@
 const display = document.getElementById("display");
 
 function appendToDisplay(input){
-
+    display.value += input;
 }
-function calculate(){
 
-}
 function clearDisplay(){
-    
+    display.value = "";
+}
+
+function calculate(){
+    try{
+        display.value = eval(display.value);
+    }
+    catch(error){
+        display.value = "Error";
+
+    }
 }
